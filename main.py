eabcaef2d6383db5377693e134d517090964617f
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2023-10-06 22:56:05 -0600

    new UI

--- main.py
+++ main.py
@@ -8,57 +8,73 @@ from build.yaplParser import yaplParser
 from yaplWalker import yaplWalker
 from yaplErrorListener import yaplErrorListener
 from prettytable import PrettyTable
+from tkinter import ttk  # <-- Importar ttk
+
 
 class YaplAnalyzerApp:
     def __init__(self, window):
         self.window = window
-        self.window.title('Analizador Semántico')
-        self.window.geometry("500x200")  # Tamaño de ventana ajustado
+        self.window.title('Proyecto')
+        self.window.geometry("900x700")  # Ajusta a tu preferencia
+        self.window.configure(bg="#f5f5f5")  # Color de fondo más suave
 
         self.run_main = tk.BooleanVar()
 
         self.create_widgets()
 
+         # Establecer un estilo para los botones
+        self.style = ttk.Style()
+        self.style.configure("TButton", foreground="white", background="#4f4f4f")
+        self.create_widgets()
+
+    
+    
+
+
+
     def create_widgets(self):
-        adharbtn = Button(
+        adharbtn = ttk.Button(  # <-- Cambiar Button por ttk.Button
             self.window,
             text='Choose File',
-            command=self.open_file
+            command=self.open_file,
         )
 
-        self.runbtn = Button(
+        self.runbtn = ttk.Button(  # <-- Cambiar Button por ttk.Button
             self.window,
             text='Run',
-            state="disabled",
-            command=self.run_analysis
+            command=self.run_analysis,
         )
 
-        clearbtn = Button(
+        clearbtn = ttk.Button(  # <-- Cambiar Button por ttk.Button
             self.window,
             text='Clear',
-            command=self.clear_text_areas
+            command=self.clear_text_areas,
         )
 
-        self.label_file_explorer = Label(self.window, text=" ", height=2, fg="blue")
+        self.label_file_explorer = Label(self.window, text=" ", height=2, fg="blue", bg="#f5f5f5")
         self.text_area_code = Text(self.window, width=80, height=20, font=("Times New Roman", 15), foreground="black", highlightthickness=0, bg="lightgray")
+
+        rolly_code = Scrollbar(self.window, orient=tk.VERTICAL, command=self.text_area_code.yview)
+        self.text_area_code['yscrollcommand'] = rolly_code.set
+        rolly_code.place(x=650, y=50, height=400) 
+
+        self.line_numbers = self.LineNumbers(self.window, self.text_area_code, width=5, height=20, font=("Times New Roman", 15), foreground="gray", highlightthickness=0, bg="#f5f5f5")
+
         self.text_area_tac = Text(self.window, width=40, height=20, font=("Times New Roman", 15), foreground="black", highlightthickness=0, bg="lightgray")
-        self.text_area_console = Text(self.window, width=70, height=10, font=("Times New Roman", 15), foreground="green", highlightthickness=0, bg="black")
-        self.text_area_symbolT = Text(self.window, width=70, height=15, font=("Courier", 14), foreground="blue", highlightthickness=0, wrap=tk.NONE, bg="black")
-        self.line_numbers = self.LineNumbers(self.window, self.text_area_code, width=2, height=20, font=("Times New Roman", 15), foreground="gray", highlightthickness=0, bg="lightgray")
-        rolly = Scrollbar(self.window, orient=tk.VERTICAL, command=self.view_all)
-        self.text_area_code['yscrollcommand'] = rolly.set
-        self.line_numbers['yscrollcommand'] = rolly.set
-        rolly.place(x=650, y=50)
+        self.text_area_console = Text(self.window, width=70, height=3, font=("Times New Roman", 12), foreground="green", highlightthickness=0, bg="black")  # Height reducido a 3
+        self.text_area_symbolT = Text(self.window, width=70, height=27, font=("Courier", 14), foreground="blue", highlightthickness=0, wrap=tk.NONE, bg="black")  # Height incrementado a 27
 
         adharbtn.grid(row=0, column=0, padx=(10, 10), pady=(10, 10))
-        self.label_file_explorer.grid(row=0, column=1, padx=(10, 10), pady=(10, 10))
+        self.label_file_explorer.grid(row=0, column=4, padx=(10, 10), pady=(10, 10))
         self.runbtn.grid(row=0, column=2, padx=(10, 10), pady=(10, 10))
         clearbtn.grid(row=0, column=3, padx=(10, 10), pady=(10, 10))
-        self.text_area_code.grid(column=0, row=1, columnspan=2, rowspan=8, padx=(10, 10), pady=(10, 10))
-        self.text_area_tac.grid(column=2, row=1, columnspan=2, rowspan=8, padx=(10, 10), pady=(10, 10))
-        self.line_numbers.grid(column=0, row=1, padx=(10, 10), pady=(10, 10))
-        self.text_area_console.grid(column=0, row=9, columnspan=2, padx=(10, 10), pady=(10, 10))
-        self.text_area_symbolT.grid(column=2, row=9, columnspan=2, padx=(10, 10), pady=(10, 10))
+        self.line_numbers.grid(column=0, row=1, padx=(0, 0), pady=(10, 10), sticky="ns")
+        self.text_area_code.grid(column=1, row=1, columnspan=2, rowspan=8, padx=(10, 10), pady=(10, 10), sticky="nsew")
+        self.text_area_tac.grid(column=3, row=1, columnspan=2, rowspan=8, padx=(10, 10), pady=(10, 10), sticky="nsew")
+        self.text_area_console.grid(column=1, row=9, columnspan=2, padx=(10, 10), pady=(10, 10), sticky="nsew")
+        self.text_area_symbolT.grid(column=3, row=9, columnspan=2, rowspan=2, padx=(10, 10), pady=(10, 10), sticky="nsew")  # rowspan incrementado para ocupar más espacio
+
+
 
     def open_file(self):
         file_path = askopenfile(initialdir="./input", mode='r', filetypes=[('YAPL Files', '*yapl'), ("all files", "*.*")])
@@ -67,7 +83,6 @@ class YaplAnalyzerApp:
             filename_splited = file_path.name.split("/")
             filename_splited = filename_splited[len(filename_splited) - 1]
             archivo1_ = "input/" + filename_splited
-            self.label_file_explorer.configure(text="./" + filename_splited)
             content = file_path.read()
             self.text_area_code.insert(tk.INSERT, content, "\n")
             self.runbtn.config(state="normal")
@@ -79,7 +94,7 @@ class YaplAnalyzerApp:
             fetched_content = self.text_area_code.get('1.0', 'end-1c')
             f.write(fetched_content)
         self.run_main.set(True)
-        self.text_area_console.insert(tk.INSERT, "Running ...", "\n")
+        self.text_area_console.insert(tk.INSERT, "CONSOLE", "\n")
         self.analyze_code()
 
     def clear_text_areas(self):
